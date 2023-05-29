63d3b4cf5082b391d60067b171d84facfc45ea52
Ricoharu <106259530+Ricoharu@users.noreply.github.com>
2023-05-29 22:14:23 +0900

    index.html index.js styles.css 整理

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputText = document.querySelector("#add-text").value;
+  document.querySelector("#add-text").value = "";
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+};
+
+document.querySelector("#add-button").addEventListener("click", () => {
+  onClickAdd();
+});
