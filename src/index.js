7fcaba8dfc0e27615898e3bf747d3e32b9fdb61b
Ricoharu <106259530+Ricoharu@users.noreply.github.com>
2023-05-29 22:31:56 +0900

    html / js 未完了のtodo テキスト完成

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ const onClickAdd = () => {
   //テキストボックスの値を取得し初期化する
   const inputText = document.querySelector("#add-text").value;
   document.querySelector("#add-text").value = "";
+
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
-  console.log(div);
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了のリストに追加
+  document.querySelector("#incomplete-list").appendChild(div);
 };
 
 document.querySelector("#add-button").addEventListener("click", () => {
