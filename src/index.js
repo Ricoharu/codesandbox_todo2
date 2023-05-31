993ce8ba35d44f652a5216804371a19d48643a24
Ricoharu <106259530+Ricoharu@users.noreply.github.com>
2023-06-01 00:28:13 +0900

    index.js 完了したTODE作成中

--- src/index.js
+++ src/index.js
@@ -17,16 +17,29 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //押された完了ボタンの親タグ（div）を未完了リストから削除
+    finish(completeButton.parentNode);
+    //完了要素に追加する要素
+    const addTarget = completeButton.parentNode;
+    //TODE内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    //div以下は初期化
+    addTarget.textContent = null;
+
+    //
   });
 
   //button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTaret = deleteButton.parentNode;
-    document.querySelector("#incomplete-list").removeChild(deleteTaret);
+    finish(deleteButton.parentNode);
   });
+
+  //完了したTODOから完了または削除する関数
+  const finish = (target) => {
+    document.querySelector("#incomplete-list").removeChild(target);
+  };
 
   //divタグの子要素に各要素を設定
   div.appendChild(li);
@@ -35,6 +48,8 @@ const onClickAdd = () => {
 
   //未完了のリストに追加
   document.querySelector("#incomplete-list").appendChild(div);
+
+  //完了のリストに追加
 };
 
 document.querySelector("#add-button").addEventListener("click", () => {
